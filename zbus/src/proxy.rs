5c02a719ce4843a67fb4d19df8a7ea2e6232f7cb
Michael Aaron Murphy <mmstick@pm.me>
2021-10-15 22:01:04 +0200

    fix: Build on Rust 1.51.0

--- zbus/src/proxy.rs
+++ zbus/src/proxy.rs
@@ -67,7 +67,7 @@ impl<'a> std::fmt::Debug for ProxyProperties<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("ProxyProperties")
             .field("values", &self.values)
-            .finish_non_exhaustive()
+            .finish()
     }
 }
 
